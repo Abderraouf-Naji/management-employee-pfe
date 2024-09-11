d4ddcb09e49603b536fc8c807ff2f7776a987534
Abderraouf Naji <naji.abderraouf@enis.tn>
2024-09-11 08:07:02 +0100

    Update WebConfig.java

--- backend/src/main/java/com/example/config/WebConfig.java
+++ backend/src/main/java/com/example/config/WebConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-  @Override
+
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.56.128:30007") // Adjust this to your frontend origin
+                .allowedOrigins("http://192.168.56.128:30007", "http://192.168.56.128:30008") // Adjust this to your frontend origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
-
